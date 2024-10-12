8dcbc5cabb96bc9014ec19c050e732d1ee56e568
A-Gbibar <gbibaraissa@gmail.com>
2024-10-12 20:24:50 +0100

    check my file image is good

--- code/apple.py
+++ code/apple.py
@@ -8,11 +8,12 @@ class Apple:
         self.snake = snake
         self.set_pos()
         
-        self.surf = pygame.image.load(join('..', "graphics", "apple.png")).convert_alpha()
+        self.surf = pygame.image.load(join('..', 'graphics', 'apple.png')).convert_alpha()
     def set_pos(self):
         available_pos = [pygame.Vector2(x, y) for x in range(COLS) for y in range(ROWS)
                          if pygame.Vector2(x, y) not in self.snake.body]
         self.pos = choice(available_pos)
     def draw(self):
         rect = (self.pos.x * CELL_SIZE, self.pos.y * CELL_SIZE, CELL_SIZE, CELL_SIZE)
-        pygame.draw.rect(self.display_surface, "yellow", rect)
+        # pygame.draw.rect(self.display_surface, "yellow", rect)
+        self.display_surface.bit(self.surf, rect)
